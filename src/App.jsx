9ce9a17043e69b0a522207d92b638dfda7b8b4dd
Sakib hasan <sakibhasan16h@gmail.com>
2025-07-29 23:45:20 +0600

    project setup completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CakeContainer from "./container/CakeContainer"
+// import React, { useState } from 'react';
+import {
+  Button,
+  DatePicker,
+  Form,
+  Input,
+  InputNumber,
+  Select
+} from 'antd';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [form] = Form.useForm();
+  const formItemLayout = {
+    labelCol: {
+      xs: { span: 24 },
+      sm: { span: 6 },
+    },
+    wrapperCol: {
+      xs: { span: 24 },
+      sm: { span: 14 },
+    },
+  };
 
+
+  // handle save data in firebase database
+  const handleFormSubmit = (values) =>{
+    console.log(values);
+  };
+  
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <CakeContainer />
+
+      {/* form to save data in firebase database */}
+        <Form
+    {...formItemLayout}
+      form={form}
+      onFinish={handleFormSubmit}
+      style={{ maxWidth: 600 }}
+  >
+    <Form.Item 
+        label="Animal Name: " 
+        name="animalName" r
+        ules={[{ required: true, message: 'Please input!' }]}>
+      <Input />
+    </Form.Item>
+
+    <Form.Item
+      label="Available Amount: "
+      name="availableNumber"
+      rules={[{ required: true, message: 'Please input!' }]}
+    >
+      <InputNumber style={{ width: '100%' }} />
+    </Form.Item>
+
+    <Form.Item
+      label="Animal Description"
+      name="animalDescription"
+      rules={[{ required: true, message: 'Please input!' }]}
+    >
+      <Input.TextArea />
+    </Form.Item>
+
+    <Form.Item
+      label="Record Date: "
+      name="recordDate"
+      rules={[{ required: true, message: 'Please input!' }]}
+    >
+      <DatePicker />
+    </Form.Item>
+
+    <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
+      <Button type="primary" htmlType="submit">
+        Submit
+      </Button>
+    </Form.Item>
+        </Form>
+    </div>
   )
 }
 
---
+++ src/container/CakeContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CakeContainer = () => {
+    return (
+        <div className='cake-container'>
+            <h2>Number of cakes: </h2>
+            <button className='large-btn'>Buy Cake</button>
+        </div>
+    );
+};
+
+export default CakeContainer;
